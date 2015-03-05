35f723dfc04d769b4956a71e09dcf6201d06bb26
Mohamed Abushadi <Mohamed_Abushadi@ultimatesoftware.com>
2015-03-04 23:24:49 -0500

    Add support for an additional script injection

--- dpxdt/client/capture.js
+++ dpxdt/client/capture.js
@@ -266,6 +266,24 @@ page.doInject = function() {
         }
     }
 
+     if(config.onReadyInjectJs) {
+        didInject = true;
+        console.log('Injecting OnReady JS: ' + config.onReadyInjectJs);
+        var success = page.evaluate(function(config) {
+            try {
+                window.eval(config.onReadyInjectJs);
+            } catch (e) {
+                console.log('Exception running OnReady JS');
+                console.log(e.stack);
+                return false;
+            }
+            return true;
+        }, config);
+        if (!success) {
+            phantom.exit(1);
+        }
+    }
+
     if (!didInject) {
         page.doScreenshot();
     } else {
